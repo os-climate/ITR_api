97cb9b6a79ef69dd16203e09614c13e518579588
Joris Cramwinckel <joris.cramwinckel@ortec-finance.com>
2021-07-09 16:10:11 +0200

    added an example Python script for a temp score request

---
+++ example_request.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+url = "http://127.0.0.1:5000/temperature_score"
+
+payload = json.dumps({
+    "aggregate": True,
+    "aggregation_method": "WATS",
+    "data_providers": [
+        "Excel"
+    ],
+    "scopes": [
+        "S1+S2"
+    ],
+    "time_frames": [
+        "long"
+    ],
+    "include_columns": [],
+    "grouping_columns": [],
+    "default_score": 3.2,
+    "companies": [
+        {
+            "company_name": "Company A",
+            "company_id": "JP0000000001",
+            "company_isin": "JP0000000001",
+            "investment_value": 35000000
+        },
+        {
+            "company_name": "Company B",
+            "company_id": "NL0000000002",
+            "company_isin": "NL0000000002",
+            "investment_value": 10000000
+        },
+        {
+            "company_name": "Company C",
+            "company_id": "IT0000000003",
+            "company_isin": "IT0000000003",
+            "investment_value": 10000000
+        }
+    ],
+    "anonymize_data_dump": False
+})
+headers = {
+    'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
